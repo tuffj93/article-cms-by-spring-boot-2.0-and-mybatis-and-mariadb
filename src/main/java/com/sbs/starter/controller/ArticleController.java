a9b55ac96f758e8f20aad027a3bbe995d1ed2349
ydkim93 <54792450+ydkim93@users.noreply.github.com>
2020-08-05 16:06:13 +0900

    게시물을 리스트를 자바버전과 el jstl 버전 2가지로

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -17,10 +20,11 @@ public class ArticleController {
 	ArticleService articleService;
 
 	@RequestMapping("/article/list")
-	public String showList() {
+	public String showList(Model aModel) {
 		List<Article> list = articleService.getList();
 		
 		log.info("list : " + list);
+		aModel.addAttribute("list",list);
 
 		return "article/list";
 	}
