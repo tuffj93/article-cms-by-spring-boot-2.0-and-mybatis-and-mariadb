435aa0839faa85fa1d6cac6ec66790ac7c0c3570
ydkim93 <54792450+ydkim93@users.noreply.github.com>
2020-08-05 15:42:00 +0900

    articleService, articleServiceImple, ArticleDto 생성 및 세팅, getList(0 함수를 구현(일단 가짜 데이터 리턴하도록)

---
+++ src/main/java/com/sbs/starter/dto/Article.java
@@ -0,0 +1,29 @@
+package com.sbs.starter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Article{
+	private long id;
+	private String regDate;
+	private String title;
+	private String body;
+	public Article(int i, String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+		id=i;
+		regDate=string;
+		title=string2;
+		body=string3;
+		System.out.println(id);
+		System.out.println(title);
+		System.out.println(body);
+		System.out.println(regDate);
+
+	}
+
+}
