7e736a4aeaa577f3ccbb2c347b0d22e404e67f6e
ydkim93 <54792450+ydkim93@users.noreply.github.com>
2020-08-07 15:03:19 +0900

    doADd 서비스가 INSERT에 의해 자동으로 만들어진 ID 값을 리턴하도록 처리

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,12 +1,16 @@
 package com.sbs.starter.controller;
 
 
+
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
@@ -23,13 +27,21 @@ public class ArticleController {
 	public String showList(Model aModel) {
 		List<Article> list = articleService.getList();
 		
-		log.info("list : " + list);
-		aModel.addAttribute("list",list);
+		aModel.addAttribute("list", list);
 
 		return "article/list";
 	}
+	
 	@RequestMapping("/article/add")
 	public String showAdd() {
 		return "article/add";
+	}
+	
+	@RequestMapping("/article/doAdd")
+	@ResponseBody
+	public String doAdd(@RequestParam Map<String, Object> param) {
+		long newId = articleService.add(param);
+
+		return newId +"번 게시물이 추가되었습니다.";
 	}
 }
--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -1,7 +1,8 @@
 package com.sbs.starter.service;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,28 @@ import org.springframework.stereotype.Service;
 import com.sbs.starter.dao.ArticleDao;
 import com.sbs.starter.dto.Article;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ArticleServiceImpl implements ArticleService {
 	@Autowired
 	ArticleDao articleDao;
+
+	// 이제 ArticleService는 데이터 관련해서는 모두 Dao에게 위임
+
+	@Override
 	public List<Article> getList() {
 		return articleDao.getList();
+	}
+
+	@Override
+	public long add(Map<String, Object> param) {
+		articleDao.add(param);
+		
+		BigInteger bigIntId= (BigInteger)param.get("id");
+		long newId = bigIntId.longValue();
+		
+		return newId;
 	}
 }
