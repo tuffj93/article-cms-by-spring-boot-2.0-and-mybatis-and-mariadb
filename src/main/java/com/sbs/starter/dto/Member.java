b56ae58509ea786e769ba3a1128b9f884ad0e9fb
ydkim93 <54792450+ydkim93@users.noreply.github.com>
2020-08-13 15:49:14 +0900

    회원가입 DTO 추가

---
+++ src/main/java/com/sbs/starter/dto/Member.java
@@ -0,0 +1,30 @@
+package com.sbs.starter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Member{
+	private long id;
+	private String regDate;
+	private String title;
+	private String body;
+	private long hit;
+	public Member(int i, String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+		id=i;
+		regDate=string;
+		title=string2;
+		body=string3;
+		System.out.println(id);
+		System.out.println(title+"title_test");
+		System.out.println(body);
+		System.out.println(regDate);
+
+	}
+
+}
